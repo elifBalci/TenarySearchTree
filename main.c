b4b43f8b1a083518b6d5f1d4c239b028b9fa8477
elifBalci <ebalci9949@gmail.com>
2018-12-04 23:49:50 +0300

    code cleaned

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
-/*
- *Elif BALCI 150117069
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
 struct ts_tree{
-    int data;
+    int *data;
     struct ts_tree * right, * left, *middle;
 };typedef struct ts_tree node;
 
@@ -16,9 +13,15 @@ void insert(node ** tree, int val);
 node* search(node ** tree, int val);
 void print_inorder(node * tree);
 void print_postorder(node * tree);
-int delete(node ** tree, int val);
+int delete(node ** tree, int val); //ekstra function , alternative to remove
 int removeFromList(int value);
+node* returnValues(node * tree, int value);
+node *removeNode (int key, node *p);
+node * findMax(node *p);
+node *findMaxOfLeft(node *p);
 
+
+//variables for alternative delete function which has a alternative "remove" function. They do the same in different ways.
 int number = 300;
 int list[] ;
 
@@ -35,50 +38,53 @@ int main() {
         fscanf(file,"%s", str);
         data = atoi(str);
         insert(&tree, data);
-        printf("in while number is %d\n", data);
+        //printf("in while number is %d\n", data);
         list[counter] = data;
         counter++;
     }
 
-    printf(" Press 1 to print preorder \n 2 to print inorder \n 3 to print postorder \n 4 to search a node \n  5 to delete a node ");
-    int choice;
-    scanf("%d", &choice);
+    int exit = 0;
+    while(exit == 0) {
+        printf(" Press 1 to print preorder \n 2 to print inorder \n 3 to print postorder \n 4 to search a node \n 5 to delete a node\n 6 to exit\n ");
+        int choice;
+        scanf("%d", &choice);
 
-    if (choice == 1)
-        print_preorder(tree);
-    if(choice == 2)
-        print_inorder(tree);
-    if(choice == 3)
-        print_postorder(tree);
-    if(choice == 4){
-        printf("please enter the  value to be searched ");
-        int val;
-        scanf("%d",&val);
-        node *node1;
-        node1 = search(&tree,val);
-        if(node1 != NULL){
-            printf("Node has been found.");
-            printf("node's middle is: %d, node's left is: %d, node's right is: %d \n", node1->middle->data, node1->left->data, node1->right->data);
+        if (choice == 1)
+            print_preorder(tree);
+        if (choice == 2)
+            print_inorder(tree);
+        if (choice == 3)
+            print_postorder(tree);
+        if (choice == 4) {
+            printf("please enter the  value to be searched ");
+            int val;
+            scanf("%d", &val);
+            node *node1;
+            node1 = search(&tree, val);
+            if (node1 != NULL) {
+                printf("Node has been found.");
+                printf("node's middle is: %d, node's left is: %d, node's right is: %d \n", node1->middle->data,
+                       node1->left->data, node1->right->data);
+            }
+
+        }
+        if (choice == 5) {
+            printf("please enter the  value to be delete ");
+            int val;
+            scanf("%d", &val);
+            //delete(&tree, val);
+            tree = removeNode(val, tree);
+            print_inorder(tree);
+            printf("++++++++++++++++++++++++ post order\n");
+            print_postorder(tree);
+            printf("++++++++++++++++++++++++ preorder\n");
+            print_preorder(tree);
+        }
+        if(choice == 6){
+            exit = 1;
         }
 
     }
-    if(choice == 5){
-        printf("please enter the  value to be delete ");
-        int val;
-        scanf("%d",&val);
-        delete(&tree, val);
-        print_inorder(tree);
-
-    }
-
-
-/*
-    node *node1 = malloc(sizeof(node));
-    node1 = search(&tree,40);
-    printf("node's middle is : %d ", node1->middle->data);
-    delete(&tree, 40);
-*/
-
     return 0;
 }
 
@@ -109,7 +115,8 @@ void insert(node ** tree, int val)
 
 void print_preorder(node * tree) {
     if (tree) {
-        printf("%d\n", tree->data);
+        if(tree->data != 0)
+            printf("%d\n", tree->data);
         print_preorder(tree->left);
         print_preorder(tree->middle);
         print_preorder(tree->right);
@@ -118,17 +125,19 @@ void print_preorder(node * tree) {
 
 void print_postorder(node * tree){
     if (tree) {
-        print_preorder(tree->left);
-        print_preorder(tree->middle);
-        print_preorder(tree->right);
-        printf("%d\n",tree->data);
+        print_postorder(tree->left);
+        print_postorder(tree->middle);
+        print_postorder(tree->right);
+        if(tree->data != 0)
+            printf("%d\n",tree->data);
 
     }
 }
 void print_inorder(node * tree){
     if (tree) {
         print_inorder(tree->left);
-        printf("%d\n",tree->data);
+        if(tree->data != 0)
+            printf("%d\n",tree->data);
         print_inorder(tree->middle);
         print_inorder(tree->right);
     }
@@ -141,21 +150,42 @@ node* search(node ** tree, int val)
         return NULL;
     }
 
-    if(val < (*tree)->data) {
+    if(val < (*tree)->data && (*tree)->data != NULL) {
         search(&(*tree)->left, val);
     }
-    else if(val > (*tree)->data && val< square((*tree)->data)) {
+    else if(val > (*tree)->data && val< square((*tree)->data) && (*tree)->data != NULL) {
         search(&(*tree)->middle, val);
     }
-    else if(val > square((*tree)->data)) {
+    else if(val > square((*tree)->data)  && (*tree)->data != NULL) {
         search(&(*tree)->right, val);
     }
-    else if(val == (*tree)->data)
+    else if(val == (*tree)->data  && (*tree)->data != NULL)
     {
         return *tree;
     }
 }
 
+node* returnValues(node *tree, int value){
+    int array[100] ;
+    node *newTree = search(&tree, value);
+    if (tree) {
+        returnValues(tree->left, value);
+        returnValues(tree->middle, value);
+        returnValues(tree->right, value);
+        printf("++++++++++++++++++++++++++++++++++++++++++\n");
+        printf("%d\n",tree->data);
+        if(tree->data != value)
+            insert(&newTree, tree->data);
+        /* int i =0 ;
+         while(array[i] != NULL)
+             i++;
+         array[i] = tree->data;*/
+        return  &array;
+    }
+}
+
+
+//An alternative way of removing nodes.
 int delete(node ** tree, int val){
     node *tempTree = malloc(sizeof(node));
     int number =0;
@@ -164,10 +194,82 @@ int delete(node ** tree, int val){
         insert(&tempTree, list[number]);
         number++;
     }
-    printf("HERE WE GO");
     print_preorder(tempTree);
+    node * new =returnValues(tree, val);
+    print_inorder(new);
+
 
 }
+
+
+node *removeNode (int key, node *p)
+{// returns a pointer to node replacing the node removed.
+    node *temp;
+    node *tmp2;
+
+    //Navigate to key's node
+    if (p == NULL)
+        return NULL;
+    if (key < p->data)
+        p->left = removeNode(key,p->left);
+    else if (key > p->data && key < square (p->data)) {
+        p->middle = removeNode(key, p->middle);
+    }
+    else if( key > square (p->data)){
+        p ->right =removeNode(key, p->right);
+    }
+
+    //delete
+    //if node only has one child
+    else if(p->right && !p->middle && !p->left){
+        p = p->right;
+    }
+    else if(!p->right && p->middle && !p->left){
+        p = p->middle;
+    }
+    else if(!p->right && !p->middle && p->left){
+        p = p->left;
+    }
+    //if node has more than one child
+    else if(p->left){
+        node *temp3;
+        temp3 = findMax(p);
+        p->data = temp3->data;
+        printf("it will be replaced by : %d\n", temp3->data);
+        free(temp3);
+    }
+
+    //if node is a leaf
+    else if(!p->middle && !p->left && !p->right ){
+        p = NULL;
+    }
+    return p;
+}
+
+node * findMax(node *p){
+    node *temp;
+    if (p->left) {
+        temp = p->left;
+        temp = findMaxOfLeft(p->left);
+    }
+    return temp ;
+}
+
+
+node *findMaxOfLeft(node *p){
+    node * temp;
+    temp = p;
+    while(temp->right || temp->middle){
+        if(temp->right)
+            temp = temp->right;
+        else if(temp->middle)
+            temp = temp->middle;
+    }
+
+    return temp;
+}
+
+
 int removeFromList(int value){
     int counter = 0;
     int counter2 = 0;
@@ -182,8 +284,8 @@ int removeFromList(int value){
         list[counter2] = list[counter2+1];
         counter2++;
     }
-
 }
+
 
 int square(int data) {
     return data*data;
